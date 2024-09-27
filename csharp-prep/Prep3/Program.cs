2dd7ed26267e0c466ed110be88890f12b93e2114
mi-ann <wir24001@byui.edu>
2024-09-27 16:40:31 -0300

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response; 
+        
+        do
+        {
+            /* Console.Write("What is the magic number? ");
+            string answer = Console.ReadLine();
+            int magic_number = int.Parse(answer); */
+
+            Random randomGenerator = new Random();
+            int magic_number = randomGenerator.Next(1, 101);
+            int guess_count = 0;
+            string correct = "no";
+            while (correct == "no")
+            {
+                Console.Write("What is your guess? ");
+                guess_count++;
+                string answer_2 = Console.ReadLine();
+                int guess = int.Parse(answer_2);
+                
+                if (guess < magic_number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magic_number) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    correct = "yes";
+                }
+               Console.WriteLine($"You've guessed {guess_count} times");
+            }
+
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
