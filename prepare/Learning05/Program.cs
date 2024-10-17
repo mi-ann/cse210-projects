a2ec6cda5263a370f22f65275ff9690689e40c8f
mi-ann <wir24001@byui.edu>
2024-10-16 23:30:29 -0300

    completed learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.getSummary());
+        Console.WriteLine(" ");
+
+        
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.getSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine(" ");
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment1.getSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
